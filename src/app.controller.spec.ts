72825c773d05ba738fbdc61a4d9b9d9f756d226c
Matej Kucera <matej.kucera.01@gmail.com>
2026-01-11 16:24:21 +0100

    app module tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,29 +1,27 @@
-import { Test, TestingModule } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
-describe('AppController', () => {
-  let appController: AppController
+describe('AppController (e2e)', () => {
+  let app: INestApplication
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const moduleRef = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [
-        {
-          provide: AppService,
-          useValue: {
-            getHealth: jest.fn().mockResolvedValue('OK'),
-          },
-        },
-      ],
+      providers: [AppService],
     }).compile()
 
-    appController = app.get<AppController>(AppController)
+    app = moduleRef.createNestApplication()
+    await app.init()
   })
 
-  describe('root', () => {
-    it('should return "OK"', async () => {
-      await expect(appController.getHealth()).resolves.toBe('OK')
-    })
+  afterEach(async () => {
+    await app.close()
+  })
+
+  it('GET /health returns OK', async () => {
+    await request(app.getHttpServer()).get('/health').expect(200).expect('OK')
   })
 })
